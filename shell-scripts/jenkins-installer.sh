885dcb1e0f51aa5b383622e0b24751ba3cd218d0
Rajdeep Pal <113296626+Rajdeep1311@users.noreply.github.com>
2023-08-31 16:18:46 +0530

    Create jenkins-installer.sh

---
+++ shell-scripts/jenkins-installer.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Update package list and upgrade existing packages
+sudo apt update
+sudo apt upgrade -y
+
+# Install Java
+sudo apt install openjdk-11-jdk -y
+
+# Add Jenkins repository key and source list
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+echo "deb http://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list
+
+# Update package list again
+sudo apt update
+
+# Install Jenkins
+sudo apt install jenkins -y
+
+# Start Jenkins service
+sudo systemctl start jenkins
+
+# Enable Jenkins to start on system boot
+sudo systemctl enable jenkins
+
+# Display Jenkins initial admin password
+echo "Jenkins initial admin password:"
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
